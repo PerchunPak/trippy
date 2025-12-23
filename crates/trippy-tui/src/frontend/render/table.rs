f5ba014f149d5fa0e662cc0834266fc285134a85
PerchunPak <git@perchun.it>
2025-12-23 17:10:28 +0100

    tui: fix `hops-table-row-inactive-text-color` not used

--- crates/trippy-tui/src/frontend/render/table.rs
+++ crates/trippy-tui/src/frontend/render/table.rs
@@ -99,7 +99,6 @@ fn render_table_row(
     custom_columns: &Columns,
 ) -> Row<'static> {
     let is_selected_hop = app.selected_hop().is_some_and(|h| h.ttl() == hop.ttl());
-    let is_in_round = app.tracer_data().is_in_round(hop, app.selected_flow);
     let (_, row_height) = if is_selected_hop && app.show_hop_details {
         render_hostname_with_details(app, hop, dns, geoip_lookup, config)
     } else {
@@ -120,7 +119,7 @@ fn render_table_row(
         })
         .collect();
 
-    let style = if is_in_round {
+    let style = if is_selected_hop {
         Style::default()
             .fg(config.theme.hops_table_row_active_text)
             .bg(config.theme.hops_table_row_active_bg)
